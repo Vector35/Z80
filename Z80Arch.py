337c359e14b1ffdac04360927e6771777edd1631
Andrew Lamoureux <andrew@vector35.com>
2019-08-05 11:14:11 -0400

    checkpoint #5

--- Z80Arch.py
+++ Z80Arch.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import re
+
 from binaryninja.log import log_info
 from binaryninja.architecture import Architecture
 from binaryninja.function import RegisterInfo, InstructionInfo, InstructionTextToken
@@ -67,6 +69,12 @@ class Z80(Architecture):
 
 	stack_pointer = "SP"
 
+	# internal
+	cond_strs = ['C', 'NC', 'Z', 'NZ', 'M', 'P', 'PE', 'PO']
+	reg8_strs = list('ABDHCELIR') + ['A\'', 'B\'', 'C\'', 'D\'', 'E\'', 'H\'', 'L\'', 'Flags', 'Flags\'', 'IXh', 'IXl', 'IYh', 'IYl']
+	reg16_strs = ['AF', 'BC', 'DE', 'HL', 'AF', 'AF\'', 'BC\'', 'DE\'', 'HL\'', 'IX', 'IY', 'SP', 'PC']
+	reg_strs = reg8_strs + reg16_strs
+
 	def get_instruction_info(self, data, addr):
 		(instrTxt, instrLen) = skwrapper.disasm(data, addr)
 		if instrLen == 0:
@@ -77,8 +85,50 @@ class Z80(Architecture):
 
 	def get_instruction_text(self, data, addr):
 		(instrTxt, instrLen) = skwrapper.disasm(data, addr)
-		tokens = [InstructionTextToken(InstructionTextTokenType.TextToken, instrTxt)]
-		return tokens, instrLen
+		if instrLen == 0:
+			return None
+
+		result = []
+		atoms = [t for t in re.split(r'([, ()\+])', instrTxt) if t] # delimeters kept if in capture group
+		result.append(InstructionTextToken(InstructionTextTokenType.InstructionToken, atoms[0]))
+		if atoms[1:]:
+			result.append(InstructionTextToken(InstructionTextTokenType.TextToken, ' '))
+
+		#
+		for atom in atoms[1:]:
+			if not atom or atom == ' ':
+				continue
+			# PROBLEM: cond 'C' conflicts with register C
+			# eg: "RET C" is it "RET <reg>" or "REG <cc>" ?
+			# eg: "CALL C" is it "CALL <reg>" or "CALL C,$0000" ?
+			elif atom == 'C' and atoms[0] in ['CALL','RET']:
+				# flag, condition code
+				result.append(InstructionTextToken(InstructionTextTokenType.TextToken, atom))
+			elif atom in self.reg16_strs or atom in self.reg8_strs:
+				result.append(InstructionTextToken(InstructionTextTokenType.RegisterToken, atom))
+			elif atom in self.cond_strs:
+				result.append(InstructionTextToken(InstructionTextTokenType.TextToken, atom))
+			elif atom[0] == '#':
+				result.append(InstructionTextToken(InstructionTextTokenType.IntegerToken, atom, int(atom[1:],16)))
+			elif atom[0] == '$':
+				if len(atom)==5:
+					result.append(InstructionTextToken(InstructionTextTokenType.PossibleAddressToken, atom, int(atom[1:],16)))
+				else:
+					result.append(InstructionTextToken(InstructionTextTokenType.IntegerToken, atom, int(atom[1:],16)))
+			elif atom.isdigit():
+				result.append(InstructionTextToken(InstructionTextTokenType.IntegerToken, atom, int(atom)))
+			elif atom == '(':
+				result.append(InstructionTextToken(InstructionTextTokenType.BeginMemoryOperandToken, atom))
+			elif atom == ')':
+				result.append(InstructionTextToken(InstructionTextTokenType.EndMemoryOperandToken, atom))
+			elif atom == '+':
+				result.append(InstructionTextToken(InstructionTextTokenType.TextToken, atom))
+			elif atom == ',':
+				result.append(InstructionTextToken(InstructionTextTokenType.OperandSeparatorToken, atom))
+			else:
+				raise Exception('unfamiliar token: %s from instruction %s' % (tok, instrTxt))
+
+		return result, instrLen
 
 	def get_instruction_low_level_il(self, data, addr, il):
 		return None
