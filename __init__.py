3ce28cc74cf0553be474fd99b2efa681e914fbb7
Andrew Lamoureux <andrew@vector35.com>
2019-08-22 16:41:29 -0400

    activate when ELF files opened with e_machine == 220 (EM_Z80)

--- __init__.py
+++ __init__.py
@@ -1,8 +1,16 @@
+import binaryninja
+
 from .Z80Arch import Z80
 Z80.register()
 
 from .ColecoView import ColecoView
 ColecoView.register()
 
-#from .RelView import RelView
-#RelView.register()
+from .RelView import RelView
+RelView.register()
+
+# built-in view
+EM_Z80 = 220
+binaryninja.BinaryViewType['ELF'].register_arch(EM_Z80, binaryninja.enums.Endianness.LittleEndian, binaryninja.Architecture['Z80'])
+
+
